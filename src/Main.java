d563c5430c0111c10b69435aacb1545b998206ac
Samara Dias da Silva <marcoscz329@gmail.com>
2026-02-11 17:24:39 -0300

    Exemplo simples

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       String nome = "Maria";
+       int idade = 25;
+       double altura = 1.68;
+       boolean ehEstudante = true;
+
+        System.out.println( "Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println( "É estudante: " + ehEstudante);
+
+    }
+}
